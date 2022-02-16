08bc5c904e03de96ee626fce271bf32faf6c457b
zwatotem <zwatotem@gmail.com>
2022-02-16 23:26:50 +0100

    Add poly- styling model in swift

--- expo-maps/ios/ExpoMaps/PropsData.swift
+++ expo-maps/ios/ExpoMaps/PropsData.swift
@@ -27,8 +27,36 @@ struct Point: Record {
 
 struct PolygonObject: Record {
   @Field var points: [Point] = []
+  @Field var fillColor: Int?
+  @Field var strokeColor: Int?
+  @Field var strokeWidth: Float?
+  @Field var strokePattern: [PatternItem]
+  @Field var jointType: Joint?
 }
 
 struct PolylineObject: Record {
   @Field var points: [Point] = []
+  @Field var color: Int?
+  @Field var width: Float?
+  @Field var pattern: [PatternItem]
+  @Field var jointType: Joint?
+  @Field var capType: Cap?
+}
+
+enum PatternItem {
+  case Dot
+  case Dash(Double)
+  case Gap(Double)
+}
+
+enum Joint : Int {
+  case Miter = 0
+  case Round
+  case Bevel
+}
+
+enum Cap : Int {
+  case Butt = 0
+  case Round = 1
+  case Square = 2
 }
