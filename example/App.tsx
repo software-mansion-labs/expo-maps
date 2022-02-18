de93b7ef009a31798f5807485e00d901df3603fb
Szymon Dziedzic <sz.dziedz@gmail.com>
2022-02-18 22:01:29 +0100

    fix android

--- example/App.tsx
+++ example/App.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigator from './navigators/MainNavigator';
 import { Platform } from 'react-native';
@@ -6,9 +6,19 @@ import { Providers } from 'expo-maps/build/Maps.types';
 import SettingsContainer from './components/SettingsContainer';
 import SwitchContainer from './components/SwitchContainer';
 import ProviderContext from './context/ProviderContext';
+import * as Location from 'expo-location';
 
 export default function App() {
   const [provider, setProvider] = useState<Providers>('google');
+
+  // it should be done as a part of library, just for now in example app
+  const getLocationPermissions = async () => {
+    await Location.requestForegroundPermissionsAsync();
+  };
+
+  useEffect(() => {
+    getLocationPermissions();
+  }, []);
 
   return (
     <ProviderContext.Provider value={provider}>
