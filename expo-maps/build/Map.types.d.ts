2738bbd4be26327da73de0e0ed0d0c5554a1e1db
Szymon Dziedzic <sz.dziedz@gmail.com>
2022-03-20 14:36:13 +0100

    feat: add docs for map types

--- expo-maps/build/Map.types.d.ts
+++ expo-maps/build/Map.types.d.ts
@@ -4,31 +4,89 @@ import { MarkerObject } from './Marker';
 import { PolygonObject } from './Polygon';
 import { PolylineObject } from './Polyline';
 import { CircleObject } from './Circle';
+/**
+ * Avaliable map types used in Expo Maps
+ *
+ * @value `normal` | `hybrid` | `satellite` | `terrain`
+ */
 export declare type MapTypes = 'normal' | 'hybrid' | 'satellite' | 'terrain';
+/**
+ * Prop for managing map type
+ *
+ * @ExpoMapsField mapType - see MapTypes type to learn more about avaliable map types in Expo Maps
+ */
 export declare type MapType = {
     mapType: MapTypes;
 };
+/**
+ * Prop for managing markers displayed on the map
+ *
+ * @ExpoMapsField markers - array of MarkerObjects
+ */
 export declare type Markers = {
     markers: MarkerObject[];
 };
+/**
+ * Prop for managing polygons displayed on the map
+ *
+ * @ExpoMapsField polygons - array of PolygonObjects
+ */
 export declare type Polygons = {
     polygons: PolygonObject[];
 };
+/**
+ * Prop for managing polylines displayed on the map
+ *
+ * @ExpoMapsField polylines - array of PolylineObjects
+ */
 export declare type Polylines = {
     polylines: PolylineObject[];
 };
+/**
+ * Prop for managing circles displayed on the map
+ *
+ * @ExpoMapsField circles - array of CircleObjects
+ */
 export declare type Circles = {
     circles: CircleObject[];
 };
+/**
+ * Prop for managing Google Maps styling settings
+ *
+ * @GoogleMapsField googleMapsJsonStyleString - valid Google Maps style JSON string,
+ * please use https://mapstyle.withgoogle.com to generate style JSONs
+ */
 export declare type GoogleMapsStyling = {
     googleMapsJsonStyleString: string;
 };
+/**
+ * Props for managing map gestures settings
+ *
+ * @ExpoMapsField enableRotateGestures - if `true` rotate gestures are enabled
+ *
+ * @ExpoMapsField enableScrollGestures - if `true` scroll gestures are enabled
+ *
+ * @ExpoMapsField enableTiltGestures - if `true` tilt gestures are enabled
+ *
+ * @ExpoMapsField enableZoomGestures - if `true` zoom gestures are enabled
+ */
 export declare type Gestures = {
     enableRotateGestures: boolean;
     enableScrollGestures: boolean;
     enableTiltGestures: boolean;
     enableZoomGestures: boolean;
 };
+/**
+ * Props for managing map controls settings
+ *
+ * @GoogleMapsField showZoomControls - if `true` zoom controls are visable
+ *
+ * @ExpoMapsField showCompass - if `true` compass icon is visable
+ *
+ * @GoogleMapsField showMapToolbar - if `true` map toolbar is visable
+ *
+ * @ExpoMapsField showLevelPicker - if `true` level picker is visable when avaliable
+ */
 export declare type Controls = {
     showZoomControls: boolean;
     showCompass: boolean;
@@ -36,14 +94,30 @@ export declare type Controls = {
     showMyLocationButton: boolean;
     showLevelPicker: boolean;
 };
-export declare type GoogleMapsControls = Controls;
-export declare type AppleMapsControls = Omit<Controls, 'showMapToolbar' | 'showZoomControls'>;
+declare type GoogleMapsControls = Controls;
+declare type AppleMapsControls = Omit<Controls, 'showMapToolbar' | 'showZoomControls'>;
 export declare type NativeExpoGoogleMapsViewProps = ViewProps & PropsWithChildren<MapType & GoogleMapsStyling & Gestures & Markers & Polygons & Polylines & Circles & GoogleMapsControls>;
 export declare type NativeExpoAppleMapsViewProps = ViewProps & PropsWithChildren<MapType & Gestures & Markers & Polygons & Polylines & Circles & AppleMapsControls>;
+/**
+ * Avaliable map providers
+ *
+ * @value `google` | `apple` - please note `apple` provider is only avaliable on Apple devices
+ */
 export declare type Providers = 'google' | 'apple';
+/**
+ * Prop for managing map provider
+ *
+ * @ExpoMapsField provider - provider you want to use for your map, please note `apple` provider is only avaliable on Apple devices
+ */
 export declare type Provider = {
     provider: Providers;
 };
+/**
+ * General Expo Map props
+ *
+ * Please see subtypes documentation for more details
+ */
 export declare type ExpoMapViewProps = ViewProps & PropsWithChildren<Partial<Provider & MapType & Controls & GoogleMapsStyling & Gestures>>;
 export declare type DefaultNativeExpoMapViewProps = MapType & Controls & Gestures;
 export declare type ExpoMapState = Markers & Polygons & Polylines & Circles;
+export {};
--- expo-maps/src/Map.types.ts
+++ expo-maps/src/Map.types.ts
@@ -5,32 +5,79 @@ import { PolygonObject } from './Polygon';
 import { PolylineObject } from './Polyline';
 import { CircleObject } from './Circle';
 
+/**
+ * Avaliable map types used in Expo Maps
+ *
+ * @value `normal` | `hybrid` | `satellite` | `terrain`
+ */
 export type MapTypes = 'normal' | 'hybrid' | 'satellite' | 'terrain';
 
+/**
+ * Prop for managing map type
+ *
+ * @ExpoMapsField mapType - see MapTypes type to learn more about avaliable map types in Expo Maps
+ */
 export type MapType = {
   mapType: MapTypes;
 };
 
+/**
+ * Prop for managing markers displayed on the map
+ *
+ * @ExpoMapsField markers - array of MarkerObjects
+ */
 export type Markers = {
   markers: MarkerObject[];
 };
 
+/**
+ * Prop for managing polygons displayed on the map
+ *
+ * @ExpoMapsField polygons - array of PolygonObjects
+ */
 export type Polygons = {
   polygons: PolygonObject[];
 };
 
+/**
+ * Prop for managing polylines displayed on the map
+ *
+ * @ExpoMapsField polylines - array of PolylineObjects
+ */
 export type Polylines = {
   polylines: PolylineObject[];
 };
 
+/**
+ * Prop for managing circles displayed on the map
+ *
+ * @ExpoMapsField circles - array of CircleObjects
+ */
 export type Circles = {
   circles: CircleObject[];
 };
 
+/**
+ * Prop for managing Google Maps styling settings
+ *
+ * @GoogleMapsField googleMapsJsonStyleString - valid Google Maps style JSON string,
+ * please use https://mapstyle.withgoogle.com to generate style JSONs
+ */
 export type GoogleMapsStyling = {
   googleMapsJsonStyleString: string;
 };
 
+/**
+ * Props for managing map gestures settings
+ *
+ * @ExpoMapsField enableRotateGestures - if `true` rotate gestures are enabled
+ *
+ * @ExpoMapsField enableScrollGestures - if `true` scroll gestures are enabled
+ *
+ * @ExpoMapsField enableTiltGestures - if `true` tilt gestures are enabled
+ *
+ * @ExpoMapsField enableZoomGestures - if `true` zoom gestures are enabled
+ */
 export type Gestures = {
   enableRotateGestures: boolean;
   enableScrollGestures: boolean;
@@ -38,6 +85,17 @@ export type Gestures = {
   enableZoomGestures: boolean;
 };
 
+/**
+ * Props for managing map controls settings
+ *
+ * @GoogleMapsField showZoomControls - if `true` zoom controls are visable
+ *
+ * @ExpoMapsField showCompass - if `true` compass icon is visable
+ *
+ * @GoogleMapsField showMapToolbar - if `true` map toolbar is visable
+ *
+ * @ExpoMapsField showLevelPicker - if `true` level picker is visable when avaliable
+ */
 export type Controls = {
   showZoomControls: boolean;
   showCompass: boolean;
@@ -46,9 +104,9 @@ export type Controls = {
   showLevelPicker: boolean;
 };
 
-export type GoogleMapsControls = Controls;
+type GoogleMapsControls = Controls;
 
-export type AppleMapsControls = Omit<
+type AppleMapsControls = Omit<
   Controls,
   'showMapToolbar' | 'showZoomControls'
 >;
@@ -76,12 +134,27 @@ export type NativeExpoAppleMapsViewProps = ViewProps &
       AppleMapsControls
   >;
 
+/**
+ * Avaliable map providers
+ *
+ * @value `google` | `apple` - please note `apple` provider is only avaliable on Apple devices
+ */
 export type Providers = 'google' | 'apple';
 
+/**
+ * Prop for managing map provider
+ *
+ * @ExpoMapsField provider - provider you want to use for your map, please note `apple` provider is only avaliable on Apple devices
+ */
 export type Provider = {
   provider: Providers;
 };
 
+/**
+ * General Expo Map props
+ *
+ * Please see subtypes documentation for more details
+ */
 export type ExpoMapViewProps = ViewProps &
   PropsWithChildren<
     Partial<Provider & MapType & Controls & GoogleMapsStyling & Gestures>
