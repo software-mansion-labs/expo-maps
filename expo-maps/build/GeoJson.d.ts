d4b2b63fa4adae240844f15194217ac34d9ce6fa
szdziedzic <sz.dziedz@gmail.com>
2022-04-16 11:24:15 +0200

    feat: Add warning about incompatible versions of iOS for Apple Maps GeoJSON

--- expo-maps/build/GeoJson.d.ts
+++ expo-maps/build/GeoJson.d.ts
@@ -115,6 +115,8 @@ export declare type GeoJsonObject = {
  *
  * See {@link GeoJsonProps} for more details.
  *
+ * Please note that GeoJSONs are only supported on Apple Maps for iOS versions >= 13.
+ *
  * On Android you can style your features individually by specifing supported properties for given feature type:
  *
  * For polygon you can use:
