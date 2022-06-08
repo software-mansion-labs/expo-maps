f5ed908a58356d5f1fb99d875d56bacd41bb3c2e
ellwii <44124878+ellwiii@users.noreply.github.com>
2022-06-08 14:10:50 +0200

    Update expo-maps/android/src/main/java/expo/modules/maps/googleMaps/placesUtils/GoogleMapsPlacesTokenUtils.kt
    Co-authored-by: Łukasz Kosmaty <kosmatylukasz@gmail.com>

--- expo-maps/android/src/main/java/expo/modules/maps/googleMaps/placesUtils/GoogleMapsPlacesTokenUtils.kt
+++ expo-maps/android/src/main/java/expo/modules/maps/googleMaps/placesUtils/GoogleMapsPlacesTokenUtils.kt
@@ -4,13 +4,13 @@ import com.google.android.libraries.places.api.model.AutocompleteSessionToken
 
 class GoogleMapsPlacesTokenUtils {
 
-    private var token = AutocompleteSessionToken.newInstance()
+  private var token = AutocompleteSessionToken.newInstance()
 
-    fun setNewSessionToken() {
-        token = AutocompleteSessionToken.newInstance()
-    }
+  fun setNewSessionToken() {
+    token = AutocompleteSessionToken.newInstance()
+  }
 
-    fun getToken(): AutocompleteSessionToken? {
-        return token
-    }
+  fun getToken(): AutocompleteSessionToken? {
+    return token
+  }
 }
