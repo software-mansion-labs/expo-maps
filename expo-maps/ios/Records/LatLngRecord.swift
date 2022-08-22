38698091ed13c0e99e3f4f6d0943e4ac2b353488
behenate <behenate@gmail.com>
2022-08-22 16:44:52 +0200

    Review and code styling updates

--- expo-maps/ios/Records/LatLngRecord.swift
+++ expo-maps/ios/Records/LatLngRecord.swift
@@ -1,13 +1,14 @@
 import ExpoModulesCore
 
-struct LatLngRecord : Record{
-    init() {}
-
-    @Field var latitude: Double?
-    @Field var longitude: Double?
-
-    init(coordinate:CLLocationCoordinate2D){
-        latitude = coordinate.latitude
-        longitude = coordinate.longitude
-    }
+struct LatLngRecord: Record {
+  init() {
+  }
+  
+  @Field var latitude: Double?
+  @Field var longitude: Double?
+  
+  init(coordinate: CLLocationCoordinate2D) {
+    latitude = coordinate.latitude
+    longitude = coordinate.longitude
+  }
 }
